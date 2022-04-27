9007d5b35b37b60b7f508e4e8ea4204704c3bd99
Marsaboys_ <95520044+DIMSUMBOYS@users.noreply.github.com>
2022-04-27 22:26:37 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,6 +1,6 @@
 # (©)Codexbotz
-# Recode by @mrismanaziz
-# Recode by @RYUUSHINNI
+# Recode by @dimsumsthd
+# Recode by @dimsumsthd
 
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
@@ -13,7 +13,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://github.com/RyuuXS/ShinFile-Sharing'>Klik Disini</a></b>\n",
+            text=f"<b>Tentang Bot ini:\n\n • Owner: @dimsumsthd\n • Channel: @cinemakita\n • Group: @friendzonepublicc\n • Source Code: <a href='https://github.com/DIMSUMBOYS/File-Sharing-Subs2'>Klik Disini</a></b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
