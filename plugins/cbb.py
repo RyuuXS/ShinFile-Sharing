c7cf78466346bc06e6d3ca2315f21e1b94b23e13
Marsaboys_ <95520044+DIMSUMBOYS@users.noreply.github.com>
2022-08-02 14:45:00 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -14,7 +14,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://github.com/DIMSUMBOYS/DIMSUMFILE-FORSUBS2'>Klik Disini</a></b>\n",
+            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{filmwibukartun}\n • Group: @{mgopublic}\n • Source Code: <a href='https://github.com/DIMSUMBOYS/DIMSUMFILE-FORSUBS2'>Klik Disini</a></b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("• CLOSE •", callback_data="close")]]
